8697b233802b869f6cdfa291831be4329c4d8502
Austin Miljour <austinmiljour@gmail.com>
2024-07-18 20:34:19 -0700

    Update PostDetail.tsx

--- client/src/views/PostDetail.tsx
+++ client/src/views/PostDetail.tsx
@@ -9,6 +9,7 @@ interface Post {
   id: number;
   title: string;
   content: string;
+  author: string;
   comments: Comment[];
 }
 
@@ -17,6 +18,7 @@ interface Comment {
   id: number;
   content: string;
   post: number;
+  author: string;
 }
 
 // Defining the PostDetail component
@@ -39,7 +41,7 @@ const PostDetail: React.FC = () => {
   // Fetching the post and comments from the API
   useEffect(() => {
     const apiUrl = import.meta.env.VITE_API_URL;
-    axios.get(`${apiUrl}/posts/${id}`)
+    axios.get(`${apiUrl}/posts/${id}/`)
       .then(response => {
         setPost(response.data);
         setComments(response.data.comments);
@@ -55,7 +57,7 @@ const PostDetail: React.FC = () => {
   // Function to handle editing the post
   const handleEditPost = () => {
     const apiUrl = import.meta.env.VITE_API_URL;
-    axios.put(`${apiUrl}/posts/${id}`, { title: editTitle, content: editContent })
+    axios.put(`${apiUrl}/posts/${id}/`, { title: editTitle, content: editContent })
       .then(response => {
         setPost(response.data);
         setIsEditing(false);
@@ -66,7 +68,7 @@ const PostDetail: React.FC = () => {
   // Function to handle deleting the post
   const handleDeletePost = () => {
     const apiUrl = import.meta.env.VITE_API_URL;
-    axios.delete(`${apiUrl}/posts/${id}`)
+    axios.delete(`${apiUrl}/posts/${id}/`)
       .then(() => navigate("/"))
       .catch(error => console.error('Error deleting post:', error));
   };
@@ -81,7 +83,7 @@ const PostDetail: React.FC = () => {
   const handleEditComment = () => {
     const apiUrl = import.meta.env.VITE_API_URL;
     if (editingCommentId !== null) {
-      axios.put(`${apiUrl}/comments/${editingCommentId}`, { content: editCommentContent, post: post!.id })
+      axios.put(`${apiUrl}/comments/${editingCommentId}/`, { content: editCommentContent, post: post!.id })
         .then(response => {
           setComments(comments.map(comment => comment.id === editingCommentId ? response.data : comment));
           setEditingCommentId(null);
@@ -94,7 +96,7 @@ const PostDetail: React.FC = () => {
   // Handle deleting the comment
   const handleDeleteComment = (commentId: number) => {
     const apiUrl = import.meta.env.VITE_API_URL;
-    axios.delete(`${apiUrl}/comments/${commentId}`)
+    axios.delete(`${apiUrl}/comments/${commentId}/`)
       .then(() => {
         setComments(comments.filter(comment => comment.id !== commentId));
       })
@@ -102,53 +104,72 @@ const PostDetail: React.FC = () => {
   };
 
   return (
-    <div>
+    <div className="px-12 text-center">
+      <h1 className="text-center text-3xl font-semibold pt-5">{post.title}</h1>
+      <p className="pb-5 text-lg">by: {post.author}</p>
+
       {isEditing ? (
-        <div>
+        <div className="flex flex-col items-center justify-center">
+          <label htmlFor="Title">Editing Title:</label>
           <input 
             type="text" 
             value={editTitle}
             onChange={(e) => setEditTitle(e.target.value)}
+            className="text-lg p-2 m-2 border border-gray-300 rounded-md w-1/3 mx-auto"
           />
+          <label htmlFor="Content">Editing Content:</label>
           <textarea 
             value={editContent}
             onChange={(e) => setEditContent(e.target.value)}
+            className="text-lg p-2 m-2 border border-gray-300 rounded-md w-2/3 mx-auto"
           />
-          <button onClick={handleEditPost}>Save</button>
-          <button onClick={() => setIsEditing(false)}>Cancel</button>
+          <div className="flex justify-around w-2/3">
+            <button onClick={handleEditPost} className="border border-black bg-green-600 rounded-md w-auto mx-auto text-lg p-2 m-2">Save Changes</button>
+            <button className="border border-black bg-red-500 rounded-md w-auto mx-auto text-lg p-2 m-2" onClick={handleDeletePost}>Delete Post</button>
+          </div>
         </div>
       ) : (
         <div>
-          <h1>{post.title}</h1>
-          <p>{post.content}</p>
-          <button onClick={() => setIsEditing(true)}>Edit Post</button>
-          <button onClick={handleDeletePost}>Delete Post</button>
+          <p className="text-xl  underling w-auto">{post.content}</p>
+          <div className="flex justify-around my-5">
+            <button className="border border-black bg-yellow-300 rounded-md w-auto mx-auto text-sm p-2 m-2" onClick={() => setIsEditing(true)}>Edit Post</button>
+          </div>
         </div>
       )}
-      <h3>Comments</h3>
+      <h3 className="text-xl font-bold">Comments</h3>
       <ul>
         {comments.map(comment => (
-          <li key={comment.id}>
+          <li key={comment.id} className="my-3 text-lg text-start">
             {editingCommentId === comment.id ? (
-              <div>
+              <div className="flex flex-col items-center justify-center">
+                <p>Comment Author: {comment.author}</p>
+                <label htmlFor="Content">Editing Comment:</label>
                 <textarea 
                   value={editCommentContent}
                   onChange={(e) => setEditCommentContent(e.target.value)}
+                  className="text-md p-2 m-2 border border-gray-300 rounded-md w-2/3 mx-auto"
                 />
-                <button onClick={handleEditComment}>Save</button>
-                <button onClick={() => setEditingCommentId(null)}>Cancel</button>
+                <div className="flex justify-around w-2/3">
+                  <button onClick={handleEditComment} className="border border-black bg-green-600 rounded-md w-auto h-10 text-sm p-2 m-2">Save</button>
+                  <button onClick={() => setEditingCommentId(null)} className="border border-black bg-yellow-300 rounded-md w-auto h-10 text-sm p-2 m-2">Cancel</button>
+                  <button className="border border-black bg-red-500 rounded-md w-auto h-10 text-sm p-2 m-2" onClick={() => handleDeleteComment(comment.id)}>Delete Comment</button>
+                </div>
               </div>
             ) : (
-              <div>
-                {comment.content}
-                <button onClick={() => startEditComment(comment.id, comment.content)}>Edit</button>
-                <button onClick={() => handleDeleteComment(comment.id)}>Delete</button>
+              <div className="flex justify-between items-center my-5">
+                <div className="flex-grow flex items-center">
+                  <h3 className="font-semibold pr-5">{comment.author}:</h3>
+                  <p className="px-10">{comment.content}</p>
+                </div>
+                <button className="border border-black bg-yellow-300 rounded-md w-auto h-10 text-sm p-2 m-2" onClick={() => startEditComment(comment.id, comment.content)}>Edit</button>
               </div>
             )}
           </li>
         ))}
       </ul>
-      <CommentForm postId={id as string} setComments={setComments} />
+      <div className="pt-10 pb-20">
+        <CommentForm postId={id as string} setComments={setComments} />
+      </div>
     </div>
   );
 }
